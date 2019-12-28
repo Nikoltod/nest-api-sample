547e9b56e376741cdae1f90307e471a54d6b1c21
Todor Nikolov <todor_nikolov@ymail.com>
2019-12-28 21:53:42 +0200

    Adding Item interface for the items.controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,18 @@
-import { Get, Post, Controller, Body } from '@nestjs/common';
+import { Get, Post, Body, Controller } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Item } from './item.interface';
 
 @Controller('items')
 export class ItemsController {
-    constructor(private readonly itemService: ItemsService) { }
-    @Get()
-    async findAll(): Promise<string[]> {
-        return this.itemService.findAll();
-    }
+  constructor(private readonly itemsService: ItemsService) {}
 
-    @Post()
-    async create(@Body() item: string) {
-        return this.itemService.create(item);
-    }
+  @Get()
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  async create(@Body() item: Item) {
+    this.itemsService.create(item);
+  }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.interface';
 
 @Injectable()
 export class ItemsService {
-    private readonly items: string[] = ['Pizza', 'Coke'];
+  private readonly items: Item[] = [];
 
-    findAll(): string[] {
-        return this.items;
-    }
+  findAll(): Item[] {
+    return this.items;
+  }
 
-    create(item: string) {
-        this.items.push(item);
-    }
+  create(item: Item) {
+    this.items.push(item);
+  }
 }
