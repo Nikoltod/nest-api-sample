3cbf0a5c175c09e3f1a4bdb1492496587f5c53a1
Todor Nikolov <todor_nikolov@ymail.com>
2019-12-28 21:41:16 +0200

    Created simple items service which returns and creates items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,16 @@
-import { Get, Post, Controller } from '@nestjs/common';
+import { Get, Post, Controller, Body } from '@nestjs/common';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
-  @Get()
-  async findAll(): Promise<string[]> {
-    return ['Pizza', 'Coke'];
-  }
+    constructor(private readonly itemService: ItemsService) { }
+    @Get()
+    async findAll(): Promise<string[]> {
+        return this.itemService.findAll();
+    }
 
-  @Post()
-  async create() {
-    return 'Not yet implemented';
-  }
+    @Post()
+    async create(@Body() item: string) {
+        return this.itemService.create(item);
+    }
 }
